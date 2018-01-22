855e5902a132052c4867d6d3dc55a21fbbc1c0d1
eleve <eleve@si-PC11>
2018-01-22 10:10:07 +0100

    Exo1 tp lundi 22/01

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,9 +1,18 @@
 //  Copyright (c) 2017 Antoine Tran Tan
-//
+//  Copyright (c) 2018 Erwan DAYOT
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
 
+
+double exo1 ( double rayon )
+{
+	double volume;
+
+	volume = 4.0 / 3.0 * 3.14 * rayon * rayon * rayon ;
+
+	return volume;
+}
 
 
 #endif // MY_HEADER_HPP
--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,17 @@
 //  Copyright (c) 2018 Antoine Tran Tan
-//
+//  Copyright (c) 2018 Erwan DAYOT , Max LYS
 
 #include <iostream>
 #include <my_header.hpp>
 
 int main()
 {
+	double r1;
+
+	r1 = exo1(10.0);
+
     // Affichage sur un terminal
     std::cout << "Hello World!" << std::endl;
-
+	
     return 0;
 }
