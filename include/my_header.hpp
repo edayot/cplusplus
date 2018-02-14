1ec410f026cfb0d1c2af4ce02b1fde5ab3e62f59
Erwan DAYOT <erwan.dayot@netcourrier.com>
2018-02-14 20:21:06 +0100

    Exo5 à tester

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+
+
 #include <cmath>
 
 int essayer_code(unsigned short code);
@@ -63,55 +66,36 @@ void exo4(void)
 		{
 			points = points + 2
 			char points;
-			sdt::cout << "Votre score augmente de 2. Votre nouveau score est de :"
-				<< points
-				<< std::end1;
-
+			printf("Votre score augmente de 2. Votre nouveau score est de : %d \n", points);
 
 		}
 		if (de = 4)
 		{
 			points = points + 4
-			char points;
-			sdt::cout << "Votre score augmente de 4. Votre nouveau score est de :"
-				<< points
-				<< std::end1;
+			printf("Votre score augmente de 4. Votre nouveau score est de : %d \n", points);
 
 		}
 		if (de = 6)
 		{
 			points = points + 6
-				char points;
-			sdt::cout << "Votre score augmente de 6. Votre nouveau score est de :"
-				<< points
-				<< std::end1;
+				printf("Votre score augmente de 6. Votre nouveau score est de : %d \n", points);
 
 		}
 		if (de = 3)
 		{
 			points = points * 2
-				char points;
-			sdt::cout << "Votre score est multiplié par 2. Votre nouveau score est de :"
-				<< points
-				<< std::end1;
-
+				printf("Votre score est multiplié par 2. Votre nouveau score est de : %d \n", points);
 		}
 		if (de = 5)
 		{
 			points = 0
-				char points;
-			sdt::cout << "Votre score est remis à zéro. Votre nouveau score est de :"
-				<< points
-				<< std::end1;
+				printf("Votre score est remis à zéro. Votre nouveau score est de : %d \n", points);
 
 		}
 		
 		if (de = 1)
 		{
-		char points;
-		sdt::cout << "Votre score est de :"
-			<< points
-			<< std::end1;
+			printf("Votre nouveau score est de : %d . La partie est terminé. \n", points);
 		test = 1;
 
 		}
@@ -119,6 +103,106 @@ void exo4(void)
 
 	}
 }
+
+void exo5(void)
+{
+	
+	int tours = 0;
+	int points_Joueur1 = 0;
+	int points Joueur2 = 0;
+	int test1 = 0;
+	int test2 = 0;
+	int test = 0;
+	int max = 50;
+	printf("La partie commence le Joueur1 commence");
+
+	while (test == 0)
+	{
+		while (test1 == 0)
+		{
+			int de = lancer_de();
+			if (de = 2)
+			{
+				points_Joueur1 = points_Joueur1 + 2
+				char points_Joueur1;
+				printf("Votre score augmente de 2. Votre nouveau score est de : %d \n", points_Joueur1);
+			}
+			if (de = 4)
+			{
+				points_Joueur1 = points_Joueur1 + 4
+				printf("Votre score augmente de 4. Votre nouveau score est de : %d \n", points_Joueur1);
+			}
+			if (de = 6)
+			{
+				points_Joueur1 = points_Joueur1 + 6
+					printf("Votre score augmente de 6. Votre nouveau score est de : %d \n", points_Joueur1);
+			}
+			if (de = 3)
+			{
+				points_Joueur1 = points_Joueur1 * 2
+					printf("Votre score est multiplié par 2. Votre nouveau score est de : %d \n", points_Joueur1);
+			}
+			if (de = 5)
+			{
+				points_Joueur1 = 0
+					printf("Votre score est remis à zéro. Votre nouveau score est de : %d \n", points_Joueur1);
+			}
+			if (de = 1)
+			{
+				printf("Votre nouveau score est de : %d . Son tour est terminé. \n", points_Joueur1);
+				test1 = 1;
+				if (points_Joueur1 >= max)
+				{
+					printf("Le Joueur1 a gagner avec : %d points.\n", points_Joueur1);
+					test = 1;
+				}
+			}
+		}
+		while (test2 == 0)
+		{
+			int de = lancer_de();
+			if (de = 2)
+			{
+				points_Joueur2 = points_Joueur2 + 2
+
+				char points_Joueur2;
+				printf("Votre score augmente de 2. Votre nouveau score est de : %d \n", points_Joueur2);
+			}
+			if (de = 4)
+			{
+				points_Joueur2 = points_Joueur2 + 4
+				printf("Votre score augmente de 4. Votre nouveau score est de : %d \n", points_Joueur2);
+			}
+			if (de = 6)
+			{
+				points_Joueur2 = points_Joueur2 + 6
+				printf("Votre score augmente de 6. Votre nouveau score est de : %d \n", points_Joueur2);
+			}
+			if (de = 3)
+			{
+				points_Joueur2 = points_Joueur2 * 2
+				printf("Votre score est multiplié par 2. Votre nouveau score est de : %d \n", points_Joueur2);
+			}
+			if (de = 5)
+			{
+				points_Joueur2 = 0
+				printf("Votre score est remis à zéro. Votre nouveau score est de : %d \n", points_Joueur2);	
+			}	
+			if (de = 1)
+			{
+				printf("Votre nouveau score est de : %d . Son tour est terminé. \n", points_Joueur2);
+				test2 = 1;
+				tour = tour + 1
+				if (points_Joueur2 >= max)
+				{
+					printf("Le Joueur1 a gagner avec : %d points.\n", points_Joueur2);
+					test = 1;
+				}
+			}
+		}
+	}
+	 printf("printf("La partie est terminé.");
+}	
 
 
 #endif // MY_HEADER_HPP
